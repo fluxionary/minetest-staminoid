1014bd6a996a2499be64547387013db4d95b0979
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-04 17:22:26 -0700

    tweak doing something detection

--- stamina_regen_effect.lua
+++ stamina_regen_effect.lua
@@ -8,7 +8,10 @@ staminoid.stamina_regen_effect = status_effects.register_effect("stamina_regen_e
 	step_every = 0.09,
 	on_step = function(self, player, value, dtime, now)
 		local controls = player:get_player_control()
-		local doing_something = controls.aux1 or controls.sneak or controls.dig or controls.place
+		local doing_something = controls.aux1
+			or (controls.sneak and (controls.up or controls.down or controls.left or controls.right))
+			or controls.dig
+			or controls.place
 		local last_exhaust_timestamp = staminoid.get_last_exhaust_timestamp(player)
 		if not doing_something and last_exhaust_timestamp + (s.stamina_regen_cooldown * 1e6) <= now then
 			local stamina = staminoid.stamina_attribute:get(player) + (value * dtime / s.stamina_regen_tick)
