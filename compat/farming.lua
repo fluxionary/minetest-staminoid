c02627f6114baf336e7f03b8a65df11570d280ac
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-19 14:25:00 -0700

    add more info to placing crops exhaustion reason

--- compat/farming.lua
+++ compat/farming.lua
@@ -5,7 +5,7 @@ local old_place_seed = farming.place_seed
 function farming.place_seed(itemstack, placer, pointed_thing, plantname)
 	local rv = old_place_seed(itemstack, placer, pointed_thing, plantname)
 	if minetest.is_player(placer) then
-		staminoid.exhaust(placer, s.exhaust_place, "place")
+		staminoid.exhaust(placer, s.exhaust_place, { type = "place", pos = pointed_thing.under })
 	end
 	return rv
 end
